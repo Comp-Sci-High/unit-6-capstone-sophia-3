1ee1ff706aa22abd7072c655d2771912e34fa149
sophi2107 <sophia.rivera25@compscihigh.org>
2025-05-13 14:16:23 +0000

    Milestone 2 done

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
+const mongoose = require("mongoose");
+const express = require("express");
 
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.json());
+
+app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+    console.log(`${req.method}: ${req.path}`);
+    next();
+});
+//Items Schema and routes 
+const itemSchema = new mongoose.Schema(
+    {
+        image: {type: String, required: true},
+        price: {type: Number, required: true, default: 20},
+        size: {type: Number,}
+    }
+);
+app.post("/addItem", async (req, res) => {
+const addItem = await new Item({
+    image: req.body.image,
+    price: req.body.price, 
+    size: req.body.size,
+}).save()
+console.log(addItem)
+res.json(addItem)
+})
+
+app.get("/items", async (req, res) => {
+    const items = await Item.find({})
+    res.render("items.ejs", {items})
+})
+//After this route go to items.ejs and input the values
+
+app.patch("/items/update/:name", async ( req, res) => {
+    const response = await Item.findOneAndUpdate({
+        image: req.params.image,
+        price: req.params.price, 
+        size: req.params.size,
+    },{
+        image: req.body.image,
+        price: req.body.price,
+        size: req.body.size,
+    })
+    res.json(response)
+})
+
+app.delete("/delete/:item", async (req, res) => {
+    const itemName = req.params.itemName
+    const response = await Item.findOneAndDelete({
+        name: req.params.name,
+    })
+    res.json(response)
+})
+const Item = mongoose.model("Item", itemSchema, "Items");
+async function startServer() {
+    // Add your SRV string, make sure that the database is called SE12
+    await mongoose.connect("mongodb+srv://SE12:CSH2025@cluster0.u9yhg.mongodb.net/CSHpets?retryWrites=true&w=majority&appName=Cluster0");
+app.listen(3000, () => {
+        console.log(`Server running.`);
+    });
+}
+
+
+startServer();
